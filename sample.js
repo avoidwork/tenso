f3511fb9104b0566eaffb39869e012c673369943
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-12-19 00:04:25 -0500

    Updating sample

--- sample.js
+++ sample.js
@@ -1,1 +1,21 @@
-require("./lib/index")({routes: require(__dirname + "/routes.js")});
+require("./lib/index")({
+	logs: {
+		level: "error",
+		stderr: true
+	},
+	auth: {
+		local: {
+			enabled: true,
+			auth: function (username, password, callback) {
+				if (username === "test" && password === 123) {
+					callback(null, {username: username, password: password});
+				}
+				else {
+					callback(true, null);
+				}
+			}
+		},
+		protect: ["/uuid"]
+	},
+	routes: require(__dirname + "/routes.js")
+});

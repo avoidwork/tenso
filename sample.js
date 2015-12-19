443491f07ad39306b41875e1d10d069f34a22c55
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-12-19 11:34:02 -0500

    Fixing the sample API, removing unneeded routes

--- sample.js
+++ sample.js
@@ -3,19 +3,5 @@ require("./lib/index")({
 		level: "info",
 		stderr: true
 	},
-	auth: {
-		local: {
-			enabled: true,
-			auth: function (username, password, callback) {
-				if (username === "test" && password === 123) {
-					callback(null, {username: username, password: password});
-				}
-				else {
-					callback(true, null);
-				}
-			}
-		},
-		protect: ["/uuid"]
-	},
 	routes: require(__dirname + "/test/routes.js")
 });

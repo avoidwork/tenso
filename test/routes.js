842c05efc1ff8ad1ebfd98d50f5b8c21423f6e02
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-07-07 16:14:23 -0400

    Updating test with a parameter route

--- test/routes.js
+++ test/routes.js
@@ -1,16 +1,56 @@
 var uuid = require("tiny-uuid4");
 
 module.exports.get = {
-	"/": ["empty", "items", "somethings", "test", "things", "users", "uuid"],
+	"/": [
+		"empty",
+		"items",
+		"somethings",
+		"test",
+		"things",
+		"users",
+		"uuid"
+	],
 	"/empty": [],
-	"/items(\/?)": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+	"/items(\/?)": [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15
+	],
 	"/test": "Get to the chopper!",
-	"/things": [{id: 1, name: "thing 1", user_id: 1}, {id: 2, name: "thing 2", user_id: 1}, {
-		id: 3,
-		name: "thing 3",
-		user_id: 2
-	}],
-	"/somethings": ["abc", "def"],
+	"/test/:hidden": function (req, res) {
+		res.send(req.params.hidden);
+	},
+	"/things": [
+		{
+			id: 1,
+			name: "thing 1",
+			user_id: 1
+		}, {
+			id: 2,
+			name: "thing 2",
+			user_id: 1
+		}, {
+			id: 3,
+			name: "thing 3",
+			user_id: 2
+		}
+	],
+	"/somethings": [
+		"abc",
+		"def"
+	],
 	"/somethings/abc": {
 		"_id": "abc",
 		"user_id": 123,
@@ -18,9 +58,19 @@ module.exports.get = {
 		"body": "Where is my body?",
 		"source_url": "http://source.tld"
 	},
-	"/somethings/def": {"_id": "def", "user_id": 123, "source_url": "http://source.tld"},
-	"/users": [123],
-	"/users/123": {"_id": 123, firstName: "Jason", lastName: "Mulligan"},
+	"/somethings/def": {
+		"_id": "def",
+		"user_id": 123,
+		"source_url": "http://source.tld"
+	},
+	"/users": [
+		123
+	],
+	"/users/123": {
+		"_id": 123,
+		firstName: "Jason",
+		lastName: "Mulligan"
+	},
 	"/uuid": function (req, res) {
 		this.respond(req, res, uuid());
 	}

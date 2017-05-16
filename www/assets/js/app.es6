ebc9acdb5acf57e7bc98f6d6b5eedb8198d3704a
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-05-15 21:28:15 -0400

    Hiding the request tab if the API is read-only

--- www/assets/js/app.es6
+++ www/assets/js/app.es6
@@ -19,6 +19,7 @@
 		"              `----'"
 	].join("\n"));
 
+	// Creating a DOM router
 	router({css: {current: "is-active", hidden: "dr-hidden"}, callback: ev => {
 		window.requestAnimationFrame(() => {
 			document.querySelectorAll("li.is-active").forEach(i => i.classList.remove("is-active"));
@@ -26,6 +27,16 @@
 		});
 	}});
 
+	// Resetting format selection (back button)
+	window.requestAnimationFrame(() => {
+		if (!(/(PATCH|PUT|POST)/).test(document.querySelector("#allow").innerText)) {
+			document.querySelector("li.request").classList.add("dr-hidden");
+		}
+
+		document.querySelector("#formats").selectedIndex = 0;
+	});
+
+	// Wiring up format selection
 	document.querySelector("#formats").onchange = function (ev) {
 		window.location = window.location.pathname + "?format=" + ev.target.options[ev.target.selectedIndex].value;
 	};
--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -3,6 +3,7 @@
 (function () {
 	console.log(["        ,----,", "      ,/   .`|", "    ,`   .'  :", "  ;    ;     /", ".'___,/    ,'              ,---,              ,---.", "|    :     |           ,-+-. /  | .--.--.    '   ,'\\", ";    |.';  ;   ,---.  ,--.'|'   |/  /    '  /   /   |", "`----'  |  |  /     \\|   |  ,\"' |  :  /`./ .   ; ,. :", "    '   :  ; /    /  |   | /  | |  :  ;_   '   | |: :", "    |   |  '.    ' / |   | |  | |\\  \\    `.'   | .; :", "    '   :  |'   ;   /|   | |  |/  `----.   \\   :    |", "    ;   |.' '   |  / |   | |--'  /  /`--'  /\\   \\  /", "    '---'   |   :    |   |/     '--'.     /  `----'", "             \\   \\  /'---'        `--'---'", "              `----'"].join("\n"));
 
+	// Creating a DOM router
 	router({ css: { current: "is-active", hidden: "dr-hidden" }, callback: function callback(ev) {
 			window.requestAnimationFrame(function () {
 				document.querySelectorAll("li.is-active").forEach(function (i) {
@@ -12,6 +13,16 @@
 			});
 		} });
 
+	// Resetting format selection (back button)
+	window.requestAnimationFrame(function () {
+		if (!/(PATCH|PUT|POST)/.test(document.querySelector("#allow").innerText)) {
+			document.querySelector("li.request").classList.add("dr-hidden");
+		}
+
+		document.querySelector("#formats").selectedIndex = 0;
+	});
+
+	// Wiring up format selection
 	document.querySelector("#formats").onchange = function (ev) {
 		window.location = window.location.pathname + "?format=" + ev.target.options[ev.target.selectedIndex].value;
 	};

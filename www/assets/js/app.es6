abde60b520b0638fa5bdc7439fc64062bb990d20
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-05-15 20:22:27 -0400

    Making the tabs work

--- www/assets/js/app.es6
+++ www/assets/js/app.es6
@@ -1,4 +1,13 @@
-function tag () {
+"use strict";
+
+(function () {
+	const now = new Date().getTime(),
+		render = window.requestAnimationFrame || function (arg) {
+			setTimeout(arg(now), 16);
+		};
+
+	//const observer = observable();
+
 	console.log([
 		"        ,----,",
 		"      ,/   .`|",
@@ -16,6 +25,22 @@ function tag () {
 		"             \\   \\  /'---'        `--'---'",
 		"              `----'"
 	].join("\n"));
-}
 
-tag();
+	router({css: {current: "is-active", hidden: "dr-hidden"}, callback: ev => {
+		render(() => {
+			document.querySelectorAll("li.is-active").forEach(i => i.classList.remove("is-active"));
+			ev.trigger.parentNode.classList.add("is-active");
+			console.log(arg.element.id, "is visible");
+		});
+	}});
+
+	// Hooking into every click event
+	//observer.hook(document.querySelector("body"), "click");
+
+	// Capturing events, and redirecting
+	/*observer.on("click", function (ev) {
+		ev.preventDefault();
+		ev.stopPropagation();
+		console.log(ev);
+	});*/
+})();
--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,8 +1,33 @@
 "use strict";
 
-function tag() {
-	console.log(["        ,----,", "      ,/   .`|", "    ,`   .'  :", "  ;    ;     /", ".'___,/    ,'              ,---,              ,---.", "|    :     |           ,-+-. /  | .--.--.    '   ,'\\", ";    |.';  ;   ,---.  ,--.'|'   |/  /    '  /   /   |", "`----'  |  |  /     \\|   |  ,\"' |  :  /`./ .   ; ,. :", "    '   :  ; /    /  |   | /  | |  :  ;_   '   | |: :", "    |   |  '.    ' / |   | |  | |\\  \\    `.'   | .; :", "    '   :  |'   ;   /|   | |  |/  `----.   \\   :    |", "    ;   |.' '   |  / |   | |--'  /  /`--'  /\\   \\  /", "    '---'   |   :    |   |/     '--'.     /  `----'", "             \\   \\  /'---'        `--'---'", "              `----'"].join("\n"));
-}
+(function () {
+	var now = new Date().getTime(),
+	    render = window.requestAnimationFrame || function (arg) {
+		setTimeout(arg(now), 16);
+	};
 
-tag();
+	//const observer = observable();
+
+	console.log(["        ,----,", "      ,/   .`|", "    ,`   .'  :", "  ;    ;     /", ".'___,/    ,'              ,---,              ,---.", "|    :     |           ,-+-. /  | .--.--.    '   ,'\\", ";    |.';  ;   ,---.  ,--.'|'   |/  /    '  /   /   |", "`----'  |  |  /     \\|   |  ,\"' |  :  /`./ .   ; ,. :", "    '   :  ; /    /  |   | /  | |  :  ;_   '   | |: :", "    |   |  '.    ' / |   | |  | |\\  \\    `.'   | .; :", "    '   :  |'   ;   /|   | |  |/  `----.   \\   :    |", "    ;   |.' '   |  / |   | |--'  /  /`--'  /\\   \\  /", "    '---'   |   :    |   |/     '--'.     /  `----'", "             \\   \\  /'---'        `--'---'", "              `----'"].join("\n"));
+
+	router({ css: { current: "is-active", hidden: "dr-hidden" }, callback: function callback(ev) {
+			render(function () {
+				document.querySelectorAll("li.is-active").forEach(function (i) {
+					return i.classList.remove("is-active");
+				});
+				ev.trigger.parentNode.classList.add("is-active");
+				console.log(arg.element.id, "is visible");
+			});
+		} });
+
+	// Hooking into every click event
+	//observer.hook(document.querySelector("body"), "click");
+
+	// Capturing events, and redirecting
+	/*observer.on("click", function (ev) {
+ 	ev.preventDefault();
+ 	ev.stopPropagation();
+ 	console.log(ev);
+ });*/
+})();
 //# sourceMappingURL=app.js.map

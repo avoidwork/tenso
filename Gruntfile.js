748e1288723e95e320c195d536f531a60f65c535
Jason Mulligan <jason.mulligan@avoidwork.com>
2018-07-08 21:45:17 -0400

    Updating router, updating plain text renderer & tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,18 @@ module.exports = function (grunt) {
 				reporter: "spec"
 			},
 			test: {
-				src: ["test/*_test.js"/*, "test/*_test2.js"*/]
+				src: [
+					"test/*_test.js"//,
+					//"test/*_test2.js"
+				]
 			}
 		},
 		watch: {
 			js: {
-				files: ["*.js", "lib/*.js"],
+				files: [
+					"*.js",
+					"lib/*.js"
+				],
 				tasks: "default"
 			}
 		}

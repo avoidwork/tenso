4ad1d88610156d4cc5dd6883e98f43fa4d2021c3
Jason Mulligan <jason.mulligan@avoidwork.com>
2018-10-22 22:55:43 -0400

    Adding `yargs` for command line properties, embedded JSON is supported

--- index.js
+++ index.js
@@ -3,10 +3,28 @@
 const fs = require("fs"),
 	path = require("path"),
 	merge = require("tiny-merge"),
+	args = require("yargs").argv,
 	root = __dirname,
 	pkg = require(path.join(root, "package.json")),
 	tenso = require(path.join(root, "lib", "tenso.js")),
-	{bootstrap} = require(path.join(root, "lib", "utility.js"));
+	{bootstrap} = require(path.join(root, "lib", "utility.js")),
+	json = /^("|\[|\{)/;
+
+// Removing default properties
+delete args._;
+delete args.$0;
+
+Object.keys(args).forEach(k => {
+	let result = args[k];
+
+	if (typeof result === "string" && json.test(result)) {
+		try {
+			args[k] = JSON.parse(result);
+		} catch (err) {
+			args[k] = result;
+		}
+	}
+});
 
 function factory (config = {}) {
 	const obj = tenso();
@@ -19,6 +37,11 @@ function factory (config = {}) {
 	obj.config.root = path.resolve(obj.config.root || __dirname);
 	obj.config.template = fs.readFileSync(config.template || path.join(obj.config.root, "template.html"), {encoding: "utf8"});
 	obj.config.version = pkg.version;
+
+	if (Object.keys(args).length > 0) {
+		merge(config, args || {});
+	}
+
 	merge(obj.config, config);
 
 	if (obj.config.silent !== true) {

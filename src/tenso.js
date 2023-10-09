e7de726c4d4e9059a2e1cb37b61977bb2f42fd6f
Jason Mulligan <jason.mulligan@avoidwork.com>
2023-10-09 17:09:33 -0400

    Updating copyright year in LICENSE & README.md, updating version in package.json, adding 'defaults' dependency & creating `./src/utils/config.js`, creating './src/tenso.js' (WIP)

---
+++ src/tenso.js
@@ -0,0 +1,24 @@
+import {Woodland} from "woodland";
+import defaults from "defaults";
+import {config as defaultConfig} from "./utils/config.js";
+
+const parsers = new Map();
+const renderers = new Map();
+const serializers = new Map();
+
+class Tenso extends Woodland {
+  constructor(config = defaultConfig) {
+	super(config);
+	this.config = config;
+	this.parsers = parsers;
+	this.rates = new Map();
+	this.renderers = renderers;
+	this.serializers = serializers;
+	this.server = null;
+	this.version = config?.version ?? version;
+  }
+}
+
+export function tenso (userConfig = {}) {
+	return new Tenso(defaults(userConfig, structuredClone(defaultConfig)));
+}
---
+++ src/utils/config.js
@@ -0,0 +1,137 @@
+import {join} from "node:path";
+import {fileURLToPath, URL} from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const {name, version} = require(join(__dirname, "..", "..", "package.json"));
+
+export const config = {
+	auth: {
+		delay: 0,
+		protect: [],
+		unprotect: [],
+		basic: {
+			enabled: false,
+			list: []
+		},
+		bearer: {
+			enabled: false,
+			tokens: []
+		},
+		jwt: {
+			enabled: false,
+			auth: null,
+			audience: "",
+			algorithms: [
+				"HS256",
+				"HS384",
+				"HS512"
+			],
+			ignoreExpiration: false,
+			issuer: "",
+			scheme: "Bearer",
+			secretOrKey: ""
+		},
+		local: {
+			enabled: false,
+			auth: null
+		},
+		msg: {
+			login: "POST 'username' & 'password' to authenticate"
+		},
+		oauth2: {
+			enabled: false,
+			auth: null,
+			auth_url: "",
+			token_url: "",
+			client_id: "",
+			client_secret: ""
+		},
+		uri: {
+			login: "/auth/login",
+			logout: "/auth/logout",
+			redirect: "/",
+			root: "/auth"
+		},
+		saml: {
+			enabled: false,
+			auth: null
+		}
+	},
+	autoindex: false,
+	cacheSize: 1e3,
+	cacheTTL: 3e5,
+	catchAll: true,
+	charset: "utf-8",
+	corsExpose: "cache-control, content-language, content-type, expires, last-modified, pragma, x-csrf-token",
+	defaultHeaders: {
+		"content-type": "application/json; charset=utf-8",
+		"vary": "accept, accept-encoding, accept-language, origin"
+	},
+	digit: 3,
+	etags: true,
+	host: "0.0.0.0",
+	index: [],
+	json: 0,
+	logging: {
+		enabled: true,
+		format: "%h %l %u %t \"%r\" %>s %b",
+		level: "debug",
+		stack: true
+	},
+	maxBytes: 0,
+	mimeType: "application/json",
+	origins: ["*"],
+	port: 8000,
+	rate: {
+		enabled: false,
+		limit: 450,
+		message: "Too many requests",
+		override: null,
+		reset: 900,
+		status: 429
+	},
+	renderHeaders: true,
+	security: {
+		key: "x-csrf-token",
+		secret: "tenso",
+		csrf: true,
+		csp: null,
+		xframe: "SAMEORIGIN",
+		p3p: "",
+		hsts: null,
+		xssProtection: true,
+		nosniff: true
+	},
+	session: {
+		cookie: {
+			httpOnly: true,
+			path: "/",
+			sameSite: true,
+			secure: "auto"
+		},
+		name: "tenso.sid",
+		proxy: true,
+		redis: {
+			host: "127.0.0.1",
+			port: 6379
+		},
+		rolling: true,
+		resave: true,
+		saveUninitialized: true,
+		secret: "tensoABC",
+		store: "memory"
+	},
+	silent: false,
+	ssl: {
+		cert: null,
+		key: null,
+		pfx: null
+	},
+	webroot: {
+		root: "",
+		static: "/assets",
+		template: ""
+	},
+	title: name,
+	version
+};

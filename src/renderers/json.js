d4cc537388eace05cb6c9580d99d8b20bd206e8c
Jason Mulligan <jason.mulligan@avoidwork.com>
2023-10-09 18:03:15 -0400

    Fixing an erroneous import

--- src/renderers/json.js
+++ src/renderers/json.js
@@ -1,4 +1,4 @@
-import { indent } from '../utils.js'
+import {indent} from "../utils/indent.js";
 
 export function json (req, res, arg) {
 	return JSON.stringify(arg, null, indent(req.headers.accept, req.server.config.json))

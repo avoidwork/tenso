112221bb1a1d2e22b745283c77d1aa3b04512aac
Jason Mulligan <jason.mulligan@avoidwork.com>
2024-07-14 20:54:09 -0400

    Minor change to 'zuul()'

--- src/middleware/zuul.js
+++ src/middleware/zuul.js
@@ -3,25 +3,25 @@ import {keymaster} from "./keymaster.js";
 
 export function zuul (req, res, next) {
 	const uri = req.parsed.pathname;
-	let protectd = false;
+	let protect = false;
 
 	if (req.unprotect === false) {
 		for (const i of req.server.config.auth.protect) {
 			if (i.test(uri)) {
-				protectd = true;
+				protect = true;
 				break;
 			}
 		}
 	}
 
 	// Setting state so the connection can be terminated properly
-	req.protect = protectd;
+	req.protect = protect;
 	req.protectAsync = false;
 
 	rate(req, res, e => {
 		if (e !== void 0) {
 			res.error(e);
-		} else if (protectd) {
+		} else if (protect) {
 			next();
 		} else {
 			keymaster(req, res);

d8702938de33ea87055d3a68fd75e2b491dcffc3
Jason Mulligan <jason.mulligan@avoidwork.com>
2024-12-10 20:23:01 -0500

    Adding a message for future me

--- src/middleware/prometheus.js
+++ src/middleware/prometheus.js
@@ -1,4 +1,4 @@
-import promBundle from "express-prom-bundle";
+import promBundle from "express-prom-bundle"; // todo replace with custom middleware to track stats
 
 // Prometheus metrics
 export const prometheus = config => promBundle(config);
